259689363d0d6b5da864df6afc1bd6278d83f0b8
ellrussbest <72700083+ellrussbest@users.noreply.github.com>
2023-08-03 18:55:40 +0200

    Update bind.cpp

--- miscellaneous/bind.cpp
+++ miscellaneous/bind.cpp
@@ -6,6 +6,19 @@ int subtract(int x, int y) {
     return x - y;
 }
 
+class MyClass {
+public:
+    MyClass() : boundMethod(std::bind(&MyClass::printMessage, this, "Hello, world!")) {}
+
+    void printMessage(const std::string& message) {
+        std::cout << message << std::endl;
+    }
+
+    std::function<void()> boundMethod;
+};
+
+
+
 int main () {
     auto x_minus_five = std::bind2nd(std::ptr_fun(subtract), 5);
     auto x_minus_five_with_placeholder = std::bind(std::ptr_fun(subtract), std::placeholders::_1, 5);
@@ -16,6 +29,15 @@ int main () {
     std::cout << x_minus_five_with_placeholder(20) << "\n"; // 15
     std::cout << x_minus_five_with_boost(20) << "\n"; // 15
     std::cout << five_minus_x(20) << "\n"; // -15
+
+    // you must pass the object you're referring to in both cases... i.e. this and myObject
+    // initialized through constructor initialization
+    MyClass myObject;
+    myObject.boundMethod();
+
+    // created through an object
+    auto boundMethod = std::bind(&MyClass::printMessage, &myObject, "Hello, world!");
+    boundMethod();
     
     return 0;
 }
