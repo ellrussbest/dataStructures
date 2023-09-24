092967fb91c8a01dcc60e92288788693935863e2
ellrussbest <ellrussbest@gmail.com>
2023-09-24 23:28:56 +0200

    insertion sort

--- ds/algorithms/insertion_sort/insertion_sort.cpp
+++ ds/algorithms/insertion_sort/insertion_sort.cpp
@@ -10,12 +10,20 @@ void insertion_sort(int* arr, int size) {
 
         while (i >= 0 && arr[i] > key)
         {
-            arr[i+1] = arr[]
+            arr[i+1] = arr[i];
+            i = i - 1;
         }
-        
+        arr[i+1] = key;
     }
 }
 
 int main () {
-    std::cout << "Hello world \n";
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int size = sizeof arr / sizeof arr[0];
+
+    insertion_sort(arr, size);
+
+    for(int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
 }
